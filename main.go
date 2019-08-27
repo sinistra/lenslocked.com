9f1db422434836da4a6300ddb21d71ab43ffb650
Paul Taylor <ptaylor@ricoh.com.au>
2019-08-28 07:49:17 +1000

    add router & routes

--- main.go
+++ main.go
@@ -2,14 +2,42 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+var port = ":3001"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+}
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Frequently asked questions.....")
+}
+
+func fourofour(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We could not find the page you "+
+		"were looking for :(</h1>"+
+		"<p>Please email us if you keep being sent to an "+
+		"invalid page.</p>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	var h http.Handler = http.HandlerFunc(fourofour)
+	r.NotFoundHandler = h
+	http.ListenAndServe(port, r)
 }
