cd31acc65f2b60683385051437eab51e622a23d6
Paul Taylor <ptaylor@ricoh.com.au>
2019-08-28 08:42:05 +1000

    add template experiment

---
+++ exp/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	data := struct {
+		Name string
+		Age  int
+	}{"John Smith",
+		42,
+	}
+
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+}
